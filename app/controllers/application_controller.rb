e475beb2f337c5edd5431d9923dc76ab4b626f3a
yuumizoi <y.mizoi@genbasupport.com>
2025-11-28 17:41:50 +0900

    パスワード再設定機能（メール送信）を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     books_path
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
