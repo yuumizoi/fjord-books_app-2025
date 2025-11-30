b401dfae8ed0a7ebcf14b357442963423474c73f
yuumizoi <y.mizoi@genbasupport.com>
2025-11-30 20:38:35 +0900

    RuboCopおよびErblintによるコードチェックと修正を完了

--- Gemfile
+++ Gemfile
@@ -71,7 +71,7 @@ group :test do
 end
 
 gem 'carrierwave'
-gem 'kaminari'
 gem 'devise'
 gem 'devise-i18n'
+gem 'kaminari'
 gem 'letter_opener_web', group: :development
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 end
