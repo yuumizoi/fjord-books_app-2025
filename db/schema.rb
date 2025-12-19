a2fb5a322b3995557b30546fa95ec3b438396986
yuumizoi <y.mizoi@genbasupport.com>
2025-12-19 16:26:34 +0900

    feat: 日報(Report)リソースの作成、およびCRUD機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_23_233449) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_19_050256) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_23_233449) do
     t.string "picture"
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,4 +75,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_23_233449) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reports", "users"
 end
