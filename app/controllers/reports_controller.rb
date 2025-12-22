bb3c538cedf808947f2ab9b19b3b565ce4a4de47
yuumizoi <y.mizoi@genbasupport.com>
2025-12-22 12:03:45 +0900

    fix: 日報作成時にログインユーザーを紐付けるように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,6 +22,7 @@ class ReportsController < ApplicationController
   # POST /reports or /reports.json
   def create
     @report = Report.new(report_params)
+    @report.user_id = current_user.id
 
     respond_to do |format|
       if @report.save
