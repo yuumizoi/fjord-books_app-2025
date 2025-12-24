9eba04ce3908d40af75c040983165f6e84872491
yuumizoi <y.mizoi@genbasupport.com>
2025-12-24 11:06:34 +0900

    style: RuboCopによる指摘箇所を修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :set_report, only: %i[show edit update destroy]
 
   # GET /reports or /reports.json
   def index
@@ -7,8 +9,7 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1 or /reports/1.json
-  def show
-  end
+  def show; end
 
   # GET /reports/new
   def new
@@ -16,8 +17,7 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /reports or /reports.json
   def create
@@ -59,13 +59,13 @@ class ReportsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def report_params
-      params.expect(report: [ :title, :body, :user_id ])
-    end
+  def set_report
+    @report = Report.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def report_params
+    params.expect(report: %i[title body user_id])
+  end
 end
