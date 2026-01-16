c45a7f443e5fa76363f07d3a7563bae3e91c4f3e
yuumizoi <y.mizoi@genbasupport.com>
2026-01-16 09:49:34 +0900

    refactor: 不要なヘルパーファイルの削除とコントローラー内の不要なコメントを修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,12 +4,12 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
   before_action :ensure_correct_user, only: %i[edit update destroy]
 
-  # GET /reports or /reports.json
+  # GET /reports
   def index
     @reports = Report.all
   end
 
-  # GET /reports/1 or /reports/1.json
+  # GET /reports/1
   def show; end
 
   # GET /reports/new
@@ -20,7 +20,7 @@ class ReportsController < ApplicationController
   # GET /reports/1/edit
   def edit; end
 
-  # POST /reports or /reports.json
+  # POST /reports
   def create
     @report = Report.new(report_params)
     @report.user_id = current_user.id
@@ -32,7 +32,7 @@ class ReportsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
+  # PATCH/PUT /reports/1
   def update
     if @report.update(report_params)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
@@ -41,7 +41,7 @@ class ReportsController < ApplicationController
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
+  # DELETE /reports/1
   def destroy
     @report.destroy!
     redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
