927f5c8a0d282eda81ea22be59aaa47eab1138a6
yuumizoi <y.mizoi@genbasupport.com>
2026-02-04 17:00:29 +0900

    test: Userモデルのname_or_emailメソッドのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "returns name when name exists" do
+    user = User.new(email: "test@example.com", name: "田中太郎")
+    assert_equal "田中太郎", user.name_or_email
+  end
+  test "returns name when name does not exist" do
+    user = User.new(email: "test@example.com", name: "")
+    assert_equal "test@example.com", user.name_or_email
+  end
 end
