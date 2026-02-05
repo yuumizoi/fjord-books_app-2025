e7b77aeb094e78445cb85f35d0c6f365e9625274
yuumizoi <y.mizoi@genbasupport.com>
2026-02-05 16:15:42 +0900

    test: 日報作成のシステムテストを追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -9,5 +9,11 @@ class ReportsTest < ApplicationSystemTestCase
     fill_in 'パスワード', with: "password123"
     click_on 'ログイン'
     assert_text 'ログインしました'
+    visit reports_path
+    click_on '日報の新規作成'
+    fill_in 'タイトル', with: "テスト日報"
+    fill_in '内容', with: "今日はテストを書きました"
+    click_on '登録する'
+    assert_text '日報が作成されました'
   end
 end
