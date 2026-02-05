cc0be4e2fcf22915d844959c333e9598c930f286
yuumizoi <y.mizoi@genbasupport.com>
2026-02-05 16:30:43 +0900

    test: 日報編集のシステムテストを追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -16,4 +16,21 @@ class ReportsTest < ApplicationSystemTestCase
     click_on '登録する'
     assert_text '日報が作成されました'
   end
+  test "user can edit report" do
+    visit new_user_session_path
+    fill_in 'Eメール', with: "one@example.com"
+    fill_in 'パスワード', with: "password123"
+    click_on 'ログイン'
+
+    visit reports_path
+    click_on '日報の新規作成'
+    fill_in 'タイトル', with: "編集前のタイトル"
+    fill_in '内容', with: "編集前の内容"
+    click_on '登録する'
+    
+    click_on 'この日報を編集'
+    fill_in 'タイトル', with: "編集後のタイトル"
+    click_on '更新する'
+    assert_text '日報が更新されました'
+  end
 end
