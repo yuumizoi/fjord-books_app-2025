dbc3e9e8452783ef12c23b62489d587aef64f6a5
yuumizoi <y.mizoi@genbasupport.com>
2026-02-05 17:46:25 +0900

    test: 日報削除のシステムテストを追加とコードリファクタリング

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -3,12 +3,16 @@
 require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
-  test "user can login and create report" do
+  def login
     visit new_user_session_path
     fill_in 'Eメール', with: "one@example.com"
     fill_in 'パスワード', with: "password123"
     click_on 'ログイン'
     assert_text 'ログインしました'
+  end
+
+  test "user can login and create report" do
+    login
     visit reports_path
     click_on '日報の新規作成'
     fill_in 'タイトル', with: "テスト日報"
@@ -16,12 +20,9 @@ class ReportsTest < ApplicationSystemTestCase
     click_on '登録する'
     assert_text '日報が作成されました'
   end
+  
   test "user can edit report" do
-    visit new_user_session_path
-    fill_in 'Eメール', with: "one@example.com"
-    fill_in 'パスワード', with: "password123"
-    click_on 'ログイン'
-
+    login
     visit reports_path
     click_on '日報の新規作成'
     fill_in 'タイトル', with: "編集前のタイトル"
@@ -32,5 +33,16 @@ class ReportsTest < ApplicationSystemTestCase
     fill_in 'タイトル', with: "編集後のタイトル"
     click_on '更新する'
     assert_text '日報が更新されました'
+  end
+
+  test "user can delete report" do
+    login
+    visit reports_path
+    click_on '日報の新規作成'
+    fill_in 'タイトル', with: "削除するタイトル"
+    fill_in '内容', with: "削除する内容"
+    click_on '登録する'
+    click_on 'この日報を削除'
+    assert_text '日報が削除されました'
   end
 end
