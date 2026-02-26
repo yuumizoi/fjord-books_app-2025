1e2194eaedb295ea3fe6a8d58ee169de460f71b6
yuumizoi <y.mizoi@genbasupport.com>
2026-02-26 10:48:09 +0900

    test: 日報更新時の言及（メンション）作成・削除の検証を追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -45,4 +45,19 @@ class ReportTest < ActiveSupport::TestCase
       report.save
     end
   end
+
+  test 'should update mentions when report content is updated' do
+    user = users(:one)
+    report = reports(:one)
+    other_report = reports(:two)
+    report_url = "http://localhost:3000/reports/#{other_report.id}"
+    
+    report.update!(content: "URLを追記しました #{report_url}")
+    assert_includes report.reload.mentioning_reports, other_report
+    assert_equal 1, report.mentioning_reports.count
+    
+    report.update!(content: "URLを消しました")
+    assert_not_includes report.reload.mentioning_reports, other_report
+    assert_equal 0, report.mentioning_reports.count
+  end
 end
